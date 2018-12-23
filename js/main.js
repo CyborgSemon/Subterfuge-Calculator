2a14caf0d2c0594ebf51535f4a53aba145f67eaf
Simon <watsonsemon@gmail.com>
2018-12-23 23:34:53 +1300

    Interface ready for functioning javascript

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
-const fab = document.querySelector('.mdc-fab');
-const dialog = new mdc.dialog.MDCDialog(document.querySelector('.assessmentDialog'));
+const your_add = document.querySelector('.your-specialists-add');
+const their_add = document.querySelector('.their-specialists-add');
 
-fab.addEventListener('click', () => {
-  dialog.open();
+const your_dialog = new mdc.dialog.MDCDialog(document.querySelector('.your-specialists'));
+const their_dialog = new mdc.dialog.MDCDialog(document.querySelector('.their-specialists'));
+
+your_add.addEventListener('click', () => {
+  your_dialog.open();
+});
+
+their_add.addEventListener('click', () => {
+  their_dialog.open();
 });
 
 
