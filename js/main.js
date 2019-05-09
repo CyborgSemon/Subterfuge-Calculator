19b881e5ecbed9e092cdd1ba02ac81afae0bf44d
Simon <watsonsemon@gmail.com>
2019-05-10 10:36:07 +1200

    small edit

--- js/main.js
+++ js/main.js
@@ -4,11 +4,11 @@ const their_add = document.querySelector('.their-specialists-add');
 const your_dialog = new mdc.dialog.MDCDialog(document.querySelector('.your-specialists'));
 const their_dialog = new mdc.dialog.MDCDialog(document.querySelector('.their-specialists'));
 
-your_add.addEventListener('click', () => {
+your_add.addEventListener('click', ()=> {
   your_dialog.open();
 });
 
-their_add.addEventListener('click', () => {
+their_add.addEventListener('click', ()=> {
   their_dialog.open();
 });
 
